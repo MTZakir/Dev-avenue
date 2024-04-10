f686b9e62a40dbc61a74ad36701ce6d013cef1ad
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-04-10 20:12:01 +0530

    random

--- Test/app.py
+++ Test/app.py
@@ -4,74 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def chat():
-    chat_info = [{
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'company',
-            'company_pic': '/static/images/netflix.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'company_name': 'Netflix',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'individual',
-            'profile_pic': '/static/images/Dave_batis.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'first_name': 'Dave',
-            'last_name': 'Batis',
-        },
-        {
-            'type': 'company',
-            'company_pic': '/static/images/netflix.png',
-            'recent_chat': 'We have sent the admin info via email to your registered email in this site please do check and let us know if you have any concerns',
-            'company_name': 'Netflix',
-        },
+    chat_info = [
         {
             'type': 'company',
             'company_pic': '/static/images/netflix.png',
@@ -79,3 +12,5 @@ def chat():
             'company_name': 'Netflix',
         },]
     return render_template('/chat.html', chat_info=chat_info)
+
+app.run()
